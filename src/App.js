94e9a28d27c9b6ad01bce8ec042299f0235d9ef0
Rodrigo Inawashiro <rodrigoinaw@hotmail.com>
2021-08-07 23:50:41 -0300

    commit falta-filtros

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Filtros } from "./components/Filtros";
+import { Produtos } from "./components/Produtos";
+import { Carrinho } from "./components/Carrinho";
+import styled from "styled-components";
+import React from "react";
+
+const AppContainer = styled.div `
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <Filtros/>
+      <Produtos/>
+      <Carrinho/>
+    </AppContainer>
+  )
 }
+
+
 
 export default App;
---
+++ src/components/Produtos.js
@@ -0,0 +1,126 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProdutosContainer = styled.div `
+   border: 1px solid black;
+   padding: 0 8px;
+`
+
+const ProdutosHeader = styled.div `
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 8px 0;
+`
+
+
+const ProdutosGrid = styled.div `
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 8px;   
+`
+
+const CardProduto = styled.div `
+    border: 1px solid black;    
+`
+
+const ImagemProduto = styled.img `
+    width: 100%;
+`
+
+const InfosProduto = styled.div `
+    padding: 8px;
+`
+
+export class Produtos extends React.Component {
+    state = {
+        produtos: [{
+            id: 1,
+            name: "Foguete da Missão Apollo 11",
+            value: 10000.00,
+            imageUrl: "https://picsum.photos/200/200",        
+            },
+            {
+            id: 2,
+            name: "Foguete da Lua 11",
+            value: 200.00,
+            imageUrl: "https://picsum.photos/200/200?a=1",        
+            },
+            {
+            id: 3,
+            name: "Foguete de Marte 11",
+            value: 100.00,
+            imageUrl: "https://picsum.photos/200/200?a=2",        
+            },
+            {
+            id: 4,
+            name: "Foguete da Terra",
+            value: 700.00,
+            imageUrl: "https://picsum.photos/200/200?a=3",        
+            },
+            {
+            id: 5,
+            name: "Foguete da Saturno",
+            value: 1100.00,
+            imageUrl: "https://picsum.photos/200/200?a=4",        
+            },
+            {
+            id: 6,
+            name: "Foguete da Jupiter",
+            value: 990.00,
+            imageUrl: "https://picsum.photos/200/200?a=5",        
+            },
+        ],
+    }; 
+
+    onClickAdicionarCarrinho = (produto) => { 
+
+        console.log("ADICIONAR AO CARRINHO", produto)
+
+    }
+
+
+    render() {
+        const produtos = this.state.produtos
+
+        console.log("PRODUTOS", this.state.produtos)
+
+        const listaDeComponentes = this.state.produtos.map((cadaProduto) => {
+            
+            return ( 
+                <CardProduto>
+                <ImagemProduto src= {cadaProduto.imageUrl} alt= {cadaProduto.name}/>
+                <InfosProduto>
+                    <p>{cadaProduto.name}</p>
+                    <p>R$ {cadaProduto.value}</p>
+                    <button onClick={() => this.onClickAdicionarCarrinho(cadaProduto)}>Adicionar ao carrinho</button>
+                </InfosProduto>
+                </CardProduto>
+            );
+        });
+
+        
+
+        return <ProdutosContainer>
+           <ProdutosHeader>
+               <p>Quantidade de Produtos: {this.state.produtos.length}</p>
+               <div>
+                   <label>
+                       Ordenação:
+                       <select>
+                           <option>Crescente</option>
+                           <option>Decrescente</option>
+                       </select>
+                   </label>
+               </div>
+           </ProdutosHeader>
+
+           <ProdutosGrid>
+               {listaDeComponentes}
+
+           </ProdutosGrid>
+
+        </ProdutosContainer>
+    }
+
+}
